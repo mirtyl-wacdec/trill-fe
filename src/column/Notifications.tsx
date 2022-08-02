8c1a4455a07ef621be62cedb1756a4281c60e5e2
s <s@s.s>
2022-08-02 12:39:04 +0900

    moar

--- src/column/Notifications.tsx
+++ src/column/Notifications.tsx
@@ -11,7 +11,7 @@ import { date_diff } from "../logic/utils";
 import Sigil from "../ui/Sigil";
 
 function Notifications() {
-  const { notifications, scryHark } = useLocalState();
+  const { notifications, scryHark, setPreview } = useLocalState();
   const [showUnread, setShowUnread] = useState(false);
   async function wipe() {
     const res = await wipeNotes();
@@ -34,15 +34,15 @@ function Notifications() {
             <Note key={JSON.stringify(n)} n={n} />
           ))}
         </div>
-        {!!notifications.unread.length && 
-        <div className="unread">
-          <p onClick={() => setShowUnread(!showUnread)}>Unread poasts</p>
-          {showUnread &&
-            notifications.unread.map((pid) => (
-              <Pid key={JSON.stringify(pid)} pid={pid} />
-            ))}
-        </div>
-}
+        {!!notifications.unread.length && (
+          <div className="unread">
+            <p onClick={() => setShowUnread(!showUnread)}>Unread poasts</p>
+            {showUnread &&
+              notifications.unread.map((pid) => (
+                <Pid key={JSON.stringify(pid)} pid={pid} />
+              ))}
+          </div>
+        )}
       </div>
     </div>
   );
@@ -51,171 +51,71 @@ function Notifications() {
 export default Notifications;
 
 interface NoteProps {
-  n: Notification
+  n: Notification;
 }
 function Note({ n }: NoteProps) {
-  const { notifications, scryHark } = useLocalState();
+  const { scryHark, setPreview } = useLocalState();
+  const [note, ship, time] =
+    "follow" in n ? (
+      [<div className="note-content">Followed you</div>, n.follow.ship, n.follow.time]
+    ) : "unfollow" in n ? (
+      [<div className="note-content">Unfollowed you</div>, n.unfollow.ship, n.unfollow.time]
+    ) : "mention" in n ? (
+      [<div className="note-content">
+        <p className="note-action">Mentioned you</p>
+        <a href={`${n.mention.pid.host}/${n.mention.pid.id}`}>in this post</a>
+      </div>, n.mention.ship, n.mention.time]
+    ) : "react" in n ? (
+      [<div className="note-content">
+        <p className="note-action">Reacted {n.react.react} to </p>
+        <a href={`${n.react.pid.host}/${n.react.pid.id}`}>your post</a>
+      </div>, n.react.ship, n.react.time]
+    ) : "reply" in n ? (
+      [<div className="note-content">
+        <a className="note-action" href={`${n.reply.ad.host}/${n.reply.ad.id}`}>
+          replied here
+        </a>
+        <a href={`${n.reply.ab.host}/${n.reply.ab.id}`}>to your post</a>
+      </div>, n.reply.ship, n.reply.time]
+    ) : "quote" in n ? (
+      [<div className="note-content">
+        <a className="note-action" href={`${n.quote.ad.host}/${n.quote.ad.id}`}>
+          quoted here
+        </a>
+        <a href={`${n.quote.ab.host}/${n.quote.ab.id}`}>your post</a>
+      </div>, n.quote.ship, n.quote.time]
+    ) : "rt" in n ? (
+      [<div className="note-content">
+        <a className="note-action" href={`${n.rt.ad.host}/${n.rt.ad.id}`}>
+          reposted here
+        </a>
+        <a href={`${n.rt.ab.host}/${n.rt.ab.id}`}>your post</a>
+      </div>, n.rt.ship, n.rt.time]
+    ) : (
+      [<></>, "", Date.now()]
+    );
   async function closeNote() {
-    const res = await dismissNote(n)
-    console.log(res, "res")
-    if (res) scryHark()
+    const res = await dismissNote(n);
+    console.log(res, "res");
+    if (res) scryHark();
   }
-  if ("follow" in n)
-    return (
-      <div className="note follow-note">
-        <div onClick={closeNote} className="close-note">
-          x
-        </div>
-        <div className="author">
-          <div className="sigil">
-            <Sigil patp={n.follow.ship} size={30} />
-          </div>
-          <p className="patp">{n.follow.ship}</p>
-        </div>
-        <div className="note-content">Followed you</div>
-        <div className="note-time">
-          <p>{date_diff(n.follow.time, "short")}</p>
-        </div>
+  return (
+    <div className="note">
+      <div onClick={closeNote} className="close-note">
+        x
       </div>
-    );
-  else if ("unfollow" in n)
-    return (
-      <div className="note unfollow-note">
-        <div onClick={closeNote} className="close-note">
-          x
+      <div onClick={() => setPreview(ship)} className="author">
+        <div className="sigil">
+          <Sigil patp={ship} size={30} />
         </div>
-        <div className="author">
-          <div className="sigil">
-            <Sigil patp={n.unfollow.ship} size={30} />
-          </div>
-          <p className="patp">{n.unfollow.ship}</p>
-        </div>
-        <div className="note-content">Unfollowed you</div>
-        <div className="note-time">
-          <p>{date_diff(n.unfollow.time, "short")}</p>
-        </div>
+        <p className="patp">{ship}</p>
       </div>
-    );
-  else if ("mention" in n)
-    return (
-      <div className="note react-note">
-        <div onClick={closeNote} className="close-note">
-          x
-        </div>
-        <div className="author">
-          <div className="sigil">
-            <Sigil patp={n.mention.ship} size={30} />
-          </div>
-          <p className="patp">{n.mention.ship}</p>
-        </div>
-        <div className="note-content">
-          <p className="note-action">Mentioned you</p>
-          <a href={`${n.mention.pid.host}/${n.mention.pid.id}`}>
-            in this post
-          </a>
-        </div>
-        <div className="note-time">
-          <p>{date_diff(n.mention.time, "short")}</p>
-        </div>
+      {note}
+      <div className="note-time">
+        <p>{date_diff(time, "short")}</p>
       </div>
-    );
-  else if ("react" in n)
-    return (
-      <div className="note react-note">
-        <div onClick={closeNote} className="close-note">
-          x
-        </div>
-        <div className="author">
-          <div className="sigil">
-            <Sigil patp={n.react.ship} size={30} />
-          </div>
-          <p className="patp">{n.react.ship}</p>
-        </div>
-        <div className="note-content">
-          <p className="note-action">Reacted {n.react.react} to </p>
-          <a href={`${n.react.pid.host}/${n.react.pid.id}`}>your post</a>
-        </div>
-        <div className="note-time">
-          <p>{date_diff(n.react.time, "short")}</p>
-        </div>
-      </div>
-    );
-  else if ("reply" in n)
-    return (
-      <div className="note reply-note">
-        <div onClick={closeNote} className="close-note">
-          x
-        </div>
-        <div className="author">
-          <div className="sigil">
-            <Sigil patp={n.reply.ship} size={30} />
-          </div>
-          <p className="patp">{n.reply.ship}</p>
-        </div>
-        <div className="note-content">
-          <a
-            className="note-action"
-            href={`${n.reply.ab.host}/${n.reply.ab.id}`}
-          >
-            replied here
-          </a>
-          <a href={`${n.reply.ad.host}/${n.reply.ad.id}`}>to your post</a>
-        </div>
-        <div className="note-time">
-          <p>{date_diff(n.reply.time, "short")}</p>
-        </div>
-      </div>
-    );
-  else if ("quote" in n)
-    return (
-      <div className="note quote-note">
-        <div onClick={closeNote} className="close-note">
-          x
-        </div>
-        <div className="author">
-          <div className="sigil">
-            <Sigil patp={n.quote.ship} size={30} />
-          </div>
-          <p className="patp">{n.quote.ship}</p>
-        </div>
-        <div className="note-content">
-          <a
-            className="note-action"
-            href={`${n.quote.ab.host}/${n.quote.ab.id}`}
-          >
-            quoted here
-          </a>
-          <a href={`${n.quote.ad.host}/${n.quote.ad.id}`}>your post</a>
-        </div>
-        <div className="note-time">
-          <p>{date_diff(n.quote.time, "short")}</p>
-        </div>
-      </div>
-    );
-  else if ("rt" in n)
-    return (
-      <div className="note repost-note">
-        <div onClick={closeNote} className="close-note">
-          x
-        </div>
-        <div className="author">
-          <div className="sigil">
-            <Sigil patp={n.rt.ship} size={30} />
-          </div>
-          <p className="patp">{n.rt.ship}</p>
-        </div>
-        <div className="note-content">
-          <a className="note-action" href={`${n.rt.ab.host}/${n.rt.ab.id}`}>
-            reposted here
-          </a>
-          <a href={`${n.rt.ad.host}/${n.rt.ad.id}`}>your post</a>
-        </div>
-        <div className="note-time">
-          <p>{date_diff(n.rt.time, "short")}</p>
-        </div>
-      </div>
-    );
-  else return <></>;
+    </div>
+  );
 }
 
 interface PidProps {
